e0cf8e9bcc247c2dd031753555250b1e969384a9
wjf <wjf@swk-mac-mini-02.local>
2018-09-07 18:54:06 +0800

    增加签到按钮

--- my-mpvue/src/pages/user/index.vue
+++ my-mpvue/src/pages/user/index.vue
@@ -10,13 +10,13 @@
     </div>
     <div class="logined" v-if="isLogin">
       <h1>Hi，{{member.name}}</h1>
-      <div class="bg-btn">
+      <div class="bg-btn" @click="toUpdataMsg()">
         <div class="vip-num">
           3939392
         </div>
       </div>
     </div>
-    <div class="vip-detail-list"  v-if="isLogin">
+    <div class="vip-detail-list" v-if="isLogin">
       <div class="detail-item">
         <div class="type">
           <span class="icon">
@@ -24,7 +24,7 @@
           </span>
           <span class="title">我的积分</span>
         </div>
-        <div class="content"><span class="integral">139</span></div>
+        <div class="content"><span class="integral">{{member.credit}}</span></div>
       </div>
       <div class="detail-item">
         <div class="type">
@@ -42,6 +42,12 @@
           </span>
           <span class="title">我的签到</span>
         </div>
+        <div class="content">
+          <span class="num">0</span>
+        </div>
+      </div>
+      <div class="signIn">
+        签到
       </div>
     </div>
     <div class="loading-container" v-if="isData">
@@ -53,7 +59,7 @@
 <script>
 import Loading from 'base/loading/loading'
 import {ERR_OK} from '@/http/config'
-import {getMemberOpenId} from '@/http/member'
+import {getMemberOpenId, checkMemberIsPay} from '@/http/member'
 export default {
   data () {
     return {
@@ -61,8 +67,10 @@ export default {
       isLogin: '',
       isData: true,
       openId: '',
+      id: '',
       member: {
-        name: ''
+        name: '',
+        credit: ''
       }
     }
   },
@@ -70,7 +78,7 @@ export default {
     Loading
   },
   onShow () {
-    // 跳转过来后没使用 onShow() 重新刷新页面
+    // 跳转过来后使用 onShow() 重新刷新页面
     this.getWxLoginResult()
   },
   onLoad () {
@@ -79,6 +87,10 @@ export default {
   methods: {
     toMemberMsg () {
       const url = '/pages/memberMsg/main?openId=' + this.openId
+      wx.navigateTo({ url })
+    },
+    toUpdataMsg () {
+      const url = '/pages/updataMemberMsg/main?id=' + this.id
       wx.navigateTo({ url })
     },
     getWxLoginResult () {
@@ -95,7 +107,7 @@ export default {
           console.log('微信登录失败，请检查网络状态')
         },
         complete (res) {
-          console.log('完成', res)
+          // console.log('完成', res)
           if (res.code) {
             wx.request({
               url: 'https://api.gcms.mygear.cn/wechat_login/get_session_info?code=' + res.code,
@@ -104,10 +116,10 @@ export default {
                 'content-type': 'json'
               },
               success: function (res) {
-                console.log('完成后的', res)
-                console.log('完成后的openID', res.data.data.openid)
+                // console.log('完成后的', res)
+                // console.log('完成后的openID', res.data.data.openid)
                 if (res.data.data.openid) {
-                  _this._getMemberOpenId(res.data.data.openid)
+                  _this._checkMemberIsPay(res.data.data.openid)
                 }
                 _this.openId = res.data.data.openid // 返回openid
               }
@@ -116,24 +128,32 @@ export default {
         }
       })
     },
+    _checkMemberIsPay (openId) {
+      checkMemberIsPay(openId).then((res) => {
+        if (res.data.data === false) {
+          this.isLogin = true
+          this.isNotLogin = false
+          this.isData = false
+          this._getMemberOpenId(openId)
+        } else {
+          this.isLogin = false
+          this.isNotLogin = true
+          this.isData = false
+        }
+      })
+    },
     _getMemberOpenId (openId) {
-      console.log(openId)
+      // console.log(openId)
       getMemberOpenId(openId).then((res) => {
-        console.log('获取openID', res)
+        // console.log('获取openID', res)
         if (res.data.code === ERR_OK) {
-          if (res.data.data.isPaid) {
-            console.log('返回的值', res.data)
-            this.isLogin = true
-            this.isNotLogin = false
-            this.member.name = res.data.data.name
-            console.log('已经登录')
-            this.isData = false
-          } else {
-            console.log('有openid,但是为支付')
-            this.isLogin = false
-            this.isNotLogin = true
-            this.isData = false
-          }
+          this.id = res.data.data.id
+          this.member.name = res.data.data.name
+          this.member.credit = res.data.data.credit
+          this.isLogin = true
+          this.isNotLogin = false
+          console.log('已经登录')
+          this.isData = false
         } else {
           console.log('没有登录')
           this.isLogin = false
@@ -188,6 +208,7 @@ export default {
         font-weight 900
         font-size $font-size-large-x
       .bg-btn
+        cursor pointer
         position relative
         height px2rem(350)
         width 84%
@@ -210,6 +231,7 @@ export default {
           left px2rem(40)
 
     .vip-detail-list
+      position relative
       margin px2rem(80) px2rem(60) px2rem(60) px2rem(60)
       .detail-item
         display flex
@@ -234,5 +256,17 @@ export default {
         color #FE335C
         .num
           color #0299FF
+        .back
+          width px2rem(20)
+          height px2rem(25)
+      .signIn
+        color $color-background
+        font-size $font-size-medium-x
+        width px2rem(80)
+        text-align center
+        padding px2rem(20) px2rem(40)
+        background #FF9800 
+        border-radius px2rem(100)
+        float right
 
 </style>
