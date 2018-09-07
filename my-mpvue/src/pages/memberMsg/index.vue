abe508454e40c294caf31e34d29b16a52ff2ea2c
wjf <wjf@swk-mac-mini-02.local>
2018-09-07 18:53:47 +0800

    修复radio问题*****

--- my-mpvue/src/pages/memberMsg/index.vue
+++ my-mpvue/src/pages/memberMsg/index.vue
@@ -6,7 +6,7 @@
           <div class="name-label">姓名</div>
         </div>
         <div class="cell-bd">
-          <input type="text" v-model="member.name" class="cell-input" placeholder="请输入名字">
+          <input type="text" v-model="member.name" class="cell-input" placeholder="请输入名字" @change="nameChange">
         </div>
       </div>
       
@@ -16,8 +16,8 @@
         </div>
         <div class="cell-bd">
           <radio-group class="radio-group" @change="radioChange">
-            <label class="radio" v-for="(item, index) in items" :key="item.name">
-              <radio :value="item.name" :checked="item.checked" color="#0299FF"/> {{item.value}}
+            <label class="radio" v-for="item in items" :key="item.name">
+              <radio :value="item.value" :checked="item.value === selectGender" color="#0299FF"/> {{item.value}}
             </label>
           </radio-group>
         </div>
@@ -35,12 +35,12 @@
     </div>
     <div class="birthday cell">
       <div class="cell-hb">
-        <div class="name-label">出生日期</div>
+        <div class="name-label">生日</div>
       </div>
       <div class="cell-bd">
         <picker mode="date"
           @value="member.birthday"
-          start="1970-01-01" :end="pickerEnd" @change="bindDateChange">
+          start="1970-01-01" :end="pickerEnd" @change="birthdayChange">
           <div class="picker">
             {{member.birthday}}
           </div>
@@ -54,36 +54,60 @@
       </div>
       <div class="cell-bd">
         <div class="cell-bd">
-          <input type="email" v-model="member.email" class="cell-input" placeholder="请输入邮箱信息">
+          <input type="email" v-model="member.email" class="cell-input" placeholder="请输入邮箱信息" @change="emailChange">
         </div>
       </div>
       <div class="cell_hb optional">选填</div>
     </div>
-    <div class="book pay">
+    <div class="pay-method cell">
+      <div class="cell-hb">
+        <div class="name-label">支付方式</div>
+      </div>
+      <div class="cell-bd">
+        <radio-group class="radio-group pay-method-item" @change="payMethodChange">
+          <label class="radio" v-for="item in payMethod" :key="item.name">
+            <radio :value="item.value" :checked="item.value === selectPayMethod" color="#0299FF"/> {{item.value}}
+          </label>
+        </radio-group>
+      </div>
+    </div>
+    <div class="book pay" v-if="isInline">
       <div class="left"><span>￥<span>{{memberPrice}}</span></span>&nbsp/&nbsp年</div>
-      <div class="right" @click="goToPay">去支付</div>
+      <div class="right" @click="goToPay">保存并支付</div>
+    </div>
+    <div class="book" v-else @click="goSave">
+      <div class="right" >保存</div>
     </div>
   </div>
 </template>
 
 <script>
 import {ERR_OK} from '@/http/config'
-import {getCourseList} from '@/http/member'
+import {addNewMember} from '@/http/member'
 import {payMent} from '@/http/pay'
 import {getOnly} from '@/http/setting'
 export default {
   data () {
     return {
+      isInline: true,
       items: [
-        {name: '男', value: '先生', checked: true},
-        {name: '女', value: '女士'}
+        {name: '男', value: '男'},
+        {name: '女', value: '女'}
       ],
+      selectGender: '男',
+      payMethod: [
+        {name: 'online', value: '线上支付'},
+        {name: 'offline', value: '线下支付'}
+      ],
+      selectPayMethod: '线上支付',
       member: {
+        openId: '',
         name: '',
         email: '',
-        gender: '男',
+        gender: '',
         telephone: '',
-        birthday: ''
+        birthday: '',
+        payMethod: ''
       },
       pickerEnd: '',
       memberPrice: ''
@@ -118,25 +142,50 @@ export default {
     },
     _getOnly () {
       getOnly().then((res) => {
+        this.member.gender = this.selectGender
+        this.member.payMethod = this.selectPayMethod
         this.memberPrice = res.data.data.memberPrice
       })
+    },
+    nameChange (e) {
+      this.member.name = e.target.value
+      console.log(this.member.name)
+    },
+    radioChange (e) {
+      console.log('radio发生change事件，携带value值为：', e.target.value)
+      console.log(e)
+      this.selectGender = e.target.value
+      this.member.gender = e.target.value
+      console.log(this.selectGender)
     },
     telephoneChange (e) {
       this.member.telephone = e.target.value
       console.log(this.member.telephone)
     },
-    radioChange (e) {
-      this.member.gender = e.target.name
-      console.log('radio发生change事件，携带value值为：', e.target.value)
+    emailChange (e) {
+      this.member.email = e.target.value
+      console.log('email', e.target.value)
     },
-    bindDateChange (e) {
+    birthdayChange (e) {
       this.member.birthday = e.target.value
       console.log('picker发送选择改变，携带值为', e.target.value)
     },
+    payMethodChange (e) {
+      console.log('payMethod', e.target.value)
+      console.log(e)
+      this.selectPayMethod = e.target.value
+      this.member.payMethod = e.target.value
+      if (this.member.payMethod === '线下支付') {
+        console.log('改变了')
+        this.isInline = false
+      } else {
+        this.isInline = true
+      }
+    },
     goToPay () {
-      console.log(this.member)
-      getCourseList(this.member).then((res) => {
-        console.log(res)
+      console.log('...', this.member)
+      addNewMember(this.member).then((res) => {
+        console.log('支付', res)
         if (res.data.data.openId) {
           payMent(res.data.data.openId).then((res) => {
             console.log('请求回来的', res)
@@ -162,6 +211,30 @@ export default {
                 fail: function (res) {
                   console.log('失败后的', res)
                   // wx.showToast({titlt: '支付失败' + res})
+                }
+              })
+            } else {
+              wx.showToast({title: '服务器忙' + res.data.code + res.data.msg})
+            }
+          })
+        }
+      })
+    },
+    goSave () {
+      console.log(this.member)
+      addNewMember(this.member).then((res) => {
+        console.log('保存', res)
+        if (res.data.data.openId) {
+          payMent(res.data.data.openId).then((res) => {
+            console.log('保存回来的', res)
+            if (res.data.code === ERR_OK) {
+              wx.showToast({
+                title: '保存成功',
+                success: function () {
+                  console.log('准备跳转')
+                  wx.switchTab({
+                    url: '/pages/user/main'
+                  })
                 }
               })
             } else {
@@ -212,8 +285,8 @@ export default {
       -ms-flex-align center
       background $color-background
       .name-label
-        font-size $font-size-medium-x
-        width px2rem(150)
+        font-size $font-size-large
+        width px2rem(160)
         word-wrap break-word
         word-break break-all
       .cell-bd
@@ -232,6 +305,15 @@ export default {
     .radio
       font-size $font-size-medium-x
       margin-right px2rem(40)
+    .pay-method
+      align-items flex-start
+      .cell-hb
+        margin-top px2rem(15)
+      .pay-method-item
+        display flex
+        flex-direction column
+        .radio
+          margin px2rem(10) 0
     .noName
       border px2rem(1) solid #f00
     .optional
