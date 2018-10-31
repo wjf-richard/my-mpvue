854636d80e32223802802fc7597677328f1eaee9
wjf <wjf@swk-mac-mini-02.local>
2018-10-31 19:13:32 +0800

    添加信息详情页面

---
+++ my-mpvue/src/base/msgDetali/msgDetali.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="msg-wrapper">
+    <h3 class="msg-title">关于我公司元旦放假时间安排</h3>
+    <p class="time">2018-10-29</p>
+    <div class="msg-main">
+      2018年10月1日至10月7日放假，共7天。<br><br>
+      10月1日(星期一)、10月2日(星期二)、10月3日(星期三)为国庆节法定节假日<br><br>
+      9月29号(星期六)、9月30号(星期天)公休调至10月4号(星期四)，10月5号(星期五)
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
